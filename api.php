abf3c2c5025ef7e9dacf19cd7d5b28a01c3f1e7f
Cristian Vega <cristianvegam@outlook.com>
2019-09-30 17:06:01 -0500

    Clean Errors

--- api.php
+++ api.php
@@ -1,8 +1,12 @@
 <?php
 require_once('rest.php');
-	
+
+/* File : rest.php
+ * Author : Arun Kumar Sekar
+ */
+
 	class API extends REST{
-    
+
     /*
     *Función que se invoca automáticamente cuando se instancia la clase API
     */
@@ -10,12 +14,12 @@ require_once('rest.php');
         //Inicializando variables locales
         parent::__construct();
     }
-    
+
     //Accediento al API
     public function processApi() {
         //Parseando la URL
         $func = strtolower(trim(str_replace("/","",$_REQUEST['rquest'])));
-        
+
         //Validando la existencia del método
         if((int)method_exists($this,$func) > 0){
             $this->$func();
@@ -23,27 +27,29 @@ require_once('rest.php');
             $this->response('',404);
         }
     }
-    
-    
-    private function users(){
-        
-        //Validando el tipo de envío
-        if($this->get_request_method() != "POST"){
+
+		//JSON Encode Array
+		private function json($data){
+				if(is_array($data)){
+						return json_encode($data);
+				}
+		}
+
+    /* TEST */
+    private function users(){ //PATH_TO_DIR/users
+
+        //Validando el tipo de solicitud
+        if($this->get_request_method() != "GET"){
             $this->response('',405);
         }
-        
-        //Define los valores recibidos
-        $type = $this->_request['type'];
-		//$this->response($this->json($result), 200);
- 
+				$result = array(
+            array("code" => "1","name" => "Josh", "birthdate" => "1983-12-08"),
+            array("code" => "2","name" => "John", "birthdate" => "1983-09-26"),
+            array("code" => "3","name" => "David", "birthdate" => "1983-11-09")
+        );
+				$this->response($this->json($result), 200);//RESPUESTA DESDE ARRAY
     }
 
-    //JSON Encode Array
-    private function json($data){
-        if(is_array($data)){
-            return json_encode($data);
-        }
-    }
 }
 
 // Inicializando API
